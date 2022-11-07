259402872f7d67aafc7145ad935e3eadd21ce7f1
FmHasibul <fmhhasan@gmail.com>
2022-11-08 00:20:54 +0600

    header added

--- src/Layouts/Main.js
+++ src/Layouts/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer';
+import Header from '../Pages/Shared/Header';
 
 const Main = () => {
     return (
         <div>
-            <h2>header</h2>
+            <Header />
             <Outlet />
             <Footer />
         </div>
